73372317875c3ef85944682805ee62c0c9026355
Lukas Mayer <lmayer@student.tgm.ac.at>
2016-03-12 17:44:53 +0100

    added initial commandpattern program

---
+++ commandPattern/server/ServerService.java
@@ -0,0 +1,14 @@
+package server;
+
+import java.rmi.RemoteException;
+
+import remoteService.DoSomethingService;
+import server.commands.Command;
+
+public class ServerService implements DoSomethingService {
+
+	@Override
+	public void doSomething(Command c) throws RemoteException {
+		c.execute();
+	}
+}
