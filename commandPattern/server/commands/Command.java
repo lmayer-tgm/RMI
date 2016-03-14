167e2b6bab4be2be7420ac888f97e13b0e56b512
Lukas Mayer <lmayer@student.tgm.ac.at>
2016-03-14 14:14:28 +0100

    documented command interface

--- commandPattern/server/commands/Command.java
+++ commandPattern/server/commands/Command.java
@@ -2,6 +2,12 @@ package server.commands;
 
 import java.io.Serializable;
 
+/**
+ * Führt eine Aufgabe aus (muss von Serializable erben!)
+ * @author lmayer
+ * @verison 12032016
+ * @param <T>
+ */
 public interface Command<T> extends Serializable {
 
 	void execute();
